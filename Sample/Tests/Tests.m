04b57d00c6d9fa6379bd8e810112930f7b74a697
akuraru <akuraru@gmail.com>
2013-11-30 13:23:57 +0900

    add tragic

---
+++ Sample/Tests/Tests.m
@@ -0,0 +1,34 @@
+//
+//  Tests.m
+//  Tests
+//
+//  Created by akuraru on 2013/11/30.
+//  Copyright (c) 2013年 P.I.akura. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertTrue(true, @"example");
+}
+
+@end
