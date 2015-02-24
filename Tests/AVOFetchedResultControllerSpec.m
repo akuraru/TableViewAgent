6a3a105daad0c6aecdaf0a9312225cb28cfcd29f
akuraru <akuraru@gmail.com>
2015-02-24 20:52:01 +0900

    よくわからんけどエラーが出てつらい

--- Tests/AVOFetchedResultControllerSpec.m
+++ Tests/AVOFetchedResultControllerSpec.m
@@ -11,9 +11,18 @@ SPEC_BEGIN(AVOFetchedResultControllerSpec)
     afterEach(^{
         [MagicalRecord cleanUp];
     });
+    describe(@"make context", ^{
+        __block NSManagedObjectContext *context;
+        beforeEach(^{
+            context = [NSManagedObjectContext MR_defaultContext];
+        });
     describe(@"meke empty controller", ^{
-        let(controller, ^NSFetchedResultsController{return [Todo MR_fetchAllGroupedBy:nil withPredicate:nil sortedBy:@"message,title" ascending:YES];});
-        let(avo, ^{return [[AVOFetchedResultController alloc] initWithFetch:controller];});
+        __block NSFetchedResultsController *controller;
+        __block AVOFetchedResultController *avo;
+        beforeEach(^{
+            controller = [Todo MR_fetchAllGroupedBy:nil withPredicate:nil sortedBy:@"message,title" ascending:YES inContext:context];
+            avo = [[AVOFetchedResultController alloc] initWithFetch:controller];
+        });
         it(@"controller is empty sectionIndexTitles", ^{
             [[[controller sectionIndexTitles] should] equal:@[]];
         });
@@ -28,16 +37,49 @@ SPEC_BEGIN(AVOFetchedResultControllerSpec)
             [[theValue([section numberOfObjects]) should] equal:theValue(0)];
             [[[section objects] should] equal:@[]];
         });
-        let(firstObject, ^{
-            Todo *todo = [Todo MR_createEntity];
-            todo.title = @"title";
-            todo.message = @"message";
-            return todo;
+    describe(@"make firstObject", ^{
+        __block Todo *firstObject;
+        beforeEach(^{
+            firstObject = [Todo MR_createInContext:context];
+            firstObject.title = @"title";
+            firstObject.message = @"message";
         });
-        it(@"do delegate TableViewAgent controller:didChangeObject:atIndexPath:forChangeType:newIndexPath:" , ^{});
-        it(@"do delegate TableViewAgent controller:didChangeSection:atIndex:forChangeType:", ^{});
-        it(@"do delegate TableViewAgent controllerWillChangeContent:", ^{});
-        it(@"do delegate TableViewAgent controllerDidChangeContent:", ^{});
-        it(@"do delegate TableViewAgent controller:sectionIndexTitleForSectionName:", ^{});
+        it(@"do delegate TableViewAgent controller:didChangeObject:atIndexPath:forChangeType:newIndexPath:" , ^{
+            id mock = [KWMock mock];
+            [[mock should] receive:@selector(controller:didChangeObject:atIndexPath:forChangeType:newIndexPath:)];
+            controller.delegate = mock;
+            [firstObject.managedObjectContext MR_saveOnlySelfAndWait];
+            [[[Todo MR_findAllInContext:context][0] should] equal:firstObject];
+        });
+        it(@"do delegate TableViewAgent controller:didChangeSection:atIndex:forChangeType:", ^{
+            id mock = [KWMock mock];
+            controller.delegate = mock;
+            [[mock should] receive:@selector(controller:didChangeSection:atIndex:forChangeType:)];
+            [firstObject.managedObjectContext MR_saveToPersistentStoreAndWait];
+            [[[Todo MR_findAllInContext:context][0] should] equal:firstObject];
+        });
+        it(@"do delegate TableViewAgent controllerWillChangeContent:", ^{
+            id mock = [KWMock mock];
+            controller.delegate = mock;
+            [[mock should] receive:@selector(controllerWillChangeContent:)];
+            [firstObject.managedObjectContext MR_saveToPersistentStoreAndWait];
+            [[[Todo MR_findAllInContext:context][0] should] equal:firstObject];
+        });
+        it(@"do delegate TableViewAgent controllerDidChangeContent:", ^{
+            id mock = [KWMock mock];
+            controller.delegate = mock;
+            [[mock should] receive:@selector(controllerDidChangeContent:)];
+            [firstObject.managedObjectContext MR_saveToPersistentStoreAndWait];
+            [[[Todo MR_findAllInContext:context][0] should] equal:firstObject];
+        });
+        it(@"do delegate TableViewAgent controller:sectionIndexTitleForSectionName:", ^{
+            id mock = [KWMock mock];
+            controller.delegate = mock;
+            [[mock should] receive:@selector(controller:sectionIndexTitleForSectionName:)];
+            [firstObject.managedObjectContext MR_saveToPersistentStoreAndWait];
+            [[[Todo MR_findAllInContext:context][0] should] equal:firstObject];
+        });
+    });
+    });
     });
 SPEC_END
